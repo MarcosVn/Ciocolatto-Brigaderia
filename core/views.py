64c0151c4f0b18a488f9e484208033003c03d6fa
MarcosVn <marcos.vnaraujo@gmail.com>
2017-09-27 22:25:43 -0300

    fixes

--- core/views.py
+++ core/views.py
@@ -13,7 +13,6 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.views.decorators.csrf import csrf_exempt
 from .models import Evento
 
-# Create your views here.
 
 def home(request):
 	eventos = Evento.objects.all().order_by('-data')[:5]
@@ -21,12 +20,10 @@ def home(request):
 	if request.method == 'POST':
 		email_usuario = request.POST.get('email')
 
-		mensagem = 'Nome: %s\n\nMensagem enviada:%s' % (request.POST.get('name'), request.POST.get('message'))
-		assunto = request.POST.get('subject')
-		
-		email = EmailMessage(assunto, mensagem, to=['jaquebelloni@gmail.com'])
+		mensagem = 'Nome: %s\nEmail: %s\nMensagem: %s' % (request.POST.get('name'), email_usuario, request.POST.get('message'))
+		assunto = request.POST.get('subject')		
+		email = EmailMessage(assunto, mensagem, email_usuario, to=['jaquebelloni@gmail.com'], headers = {'Reply-To': email_usuario})
 		email.send()
-
 
 		messages.success(request, 'Seu contato foi enviado com sucesso! Clique no x para fechar esta mensagem.')
 		return redirect(reverse('home'))
